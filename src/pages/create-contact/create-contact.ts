67cddbc51c563bfa4c16334adf0feec01d4ce11a
Raiihan <riger400@gmail.com>
2019-10-18 16:38:01 +0600

    adding contact permission

--- src/pages/create-contact/create-contact.ts
+++ src/pages/create-contact/create-contact.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
+import {Contact} from "@ionic-native/contacts";
+import {Diagnostic} from "@ionic-native/diagnostic";
 
 /**
  * Generated class for the CreateContactPage page.
@@ -15,12 +17,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreateContactPage {
   public numberOfContacts = '';
+  isContactsAuthorized: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private contacts: Contact,
+              private diagnostic: Diagnostic,
+              private platform: Platform) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateContactPage');
+  }
+
+  ionViewWillEnter() {
+    if (this.platform.is('ios')) {
+      this.diagnostic.isContactsAuthorized().then(state => {
+        this.isContactsAuthorized = state;
+        if (!state) {
+          this.requestContactsAuthorization();
+        }
+      })
+    }
+  }
+
+  requestContactsAuthorization() {
+    this.diagnostic
+      .requestContactsAuthorization()
+      .then(data => {
+        if (
+          data === this.diagnostic.permissionStatus.DENIED_ALWAYS ||
+          data === this.diagnostic.permissionStatus.DENIED
+        ) {
+          this.isContactsAuthorized = false;
+        } else {
+          this.isContactsAuthorized = true;
+        }
+      }).catch((error)=>{
+        console.log(error);
+    })
   }
 
   create() {
